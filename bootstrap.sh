7ee7983ffa1c6b68fa1073c88d3e9c0becd85d3f
fyquah95 <quah.fy95@gmail.com>
2014-09-16 17:39:33 +0800

    improved bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,24 @@
 cd $HOME
+if [ -d ".qhost" ]; then
+    echo 'You have already installed qhost! (or you simply have the directory .qhost around!'
+    exit
+fi
 git clone https://github.com/fyquah95/qhost.git
 mv qhost .qhost
 mkdir -p $HOME/bin
-ln -s $HOME/.qhost/qhost $HOME/bin/qhost
-echo "" >> $HOME/.bashrc
-echo "export PATH=\"\$PATH:$HOME/bin/\"" >> $HOME/.bashrc
+
+cd $HOME/bin
+ls | grep -q "$HOME/bin/qhost"
+if [ $? != "0" ]; then
+    ln -s $HOME/.qhost/qhost $HOME/bin/qhost
+fi
+cd $HOME
+
+echo $PATH | grep -q  "$HOME/bin"
+if [ $? != "0" ]; then
+    echo "" >> $HOME/.bashrc
+    echo "export PATH=\"\$PATH:$HOME/bin\"" >> $HOME/.bashrc
+fi
+
 source $HOME/.bashrc
-echo installation is complete!
+echo 'installation is complete!'
